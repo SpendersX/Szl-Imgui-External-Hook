a0601874818f6bbad2b0bc1332bd1626a00f464b
Kappy <143235586+SpendersX@users.noreply.github.com>
2024-07-18 07:37:51 -0400

    1.1 32bit

---
+++ Szl External Imgui Hook 32bit/loader/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClickableTransparentOverlay;
+using ImGuiNET;
+using Gui;
+
+namespace Hook
+{
+
+    class LoadHook
+    {
+        static void Main(string[] args)
+        {
+            Renderer renderer = new Renderer();
+            renderer.Start().Wait();
+        }
+    }
+}
